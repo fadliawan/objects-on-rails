3d6fc75c0ac08953ba156c3d64bca2d3453bfcd8
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2014-04-10 16:23:34 +0700

    Making new entries

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,10 +1,11 @@
 class Blog
   attr_reader :entries
+  attr_writer :post_source
 
   def initialize
     @entries = []
   end
-  
+
   def title
     "Watching Paint Dry"
   end
@@ -12,4 +13,16 @@ class Blog
   def subtitle
     "The trusted source for drying paint news & opinion"
   end
+
+  def new_post
+    post_source.call.tap do |p|
+      p.blog = self
+    end
+  end
+
+  private
+
+    def post_source
+      @post_source ||= Post.public_method(:new)
+    end
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,4 +1,5 @@
 require 'minitest/autorun'
+require 'ostruct'
 require_relative '../../app/models/blog'
 
 describe Blog do
@@ -8,5 +9,20 @@ describe Blog do
 
   it "has no entries" do
     @it.entries.must_be_empty
+  end
+
+  describe "#new_post" do
+    before do
+      @new_post = OpenStruct.new
+      @it.post_source = ->{ @new_post }
+    end
+
+    it "returns a new post" do
+      @it.new_post.must_equal @new_post
+    end
+
+    it "sets the post's blog reference to itself" do
+      @it.new_post.blog.must_equal @it
+    end
   end
 end
