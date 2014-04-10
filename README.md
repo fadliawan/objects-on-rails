41390866b51b7fab2cc95db9df3abe52f12901a5
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2014-04-10 14:58:20 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Source code from examples in http://objectsonrails.com
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloog::Application.config.secret_key_base = '7f518851712a3dd3d0d5a2998734dc4cdb2fe9e37fb9dfcf517e31aa4528c6ad27afdf9e8553f5484e6332ed4ac1deabf97f71d04943be8ef7fc14eb7a0352d9'
