f5d0cc501d3476c0ee777ccefd0011b9fea0802a
Alan K Fadliawan <alan.k.fadliawan@gmail.com>
2014-05-12 11:38:42 +0700

    Use subject and let; Add Post model

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -3,26 +3,26 @@ require 'ostruct'
 require_relative '../../app/models/blog'
 
 describe Blog do
-  before do
-    @it = Blog.new
-  end
+  subject { Blog.new }
+  let(:entries) { [] }
+
 
   it "has no entries" do
-    @it.entries.must_be_empty
+    subject.entries.must_be_empty
   end
 
   describe "#new_post" do
     before do
       @new_post = OpenStruct.new
-      @it.post_source = ->{ @new_post }
+      subject.post_source = ->{ @new_post }
     end
 
     it "returns a new post" do
-      @it.new_post.must_equal @new_post
+      subject.new_post.must_equal @new_post
     end
 
     it "sets the post's blog reference to itself" do
-      @it.new_post.blog.must_equal @it
+      subject.new_post.blog.must_equal subject
     end
   end
 end
